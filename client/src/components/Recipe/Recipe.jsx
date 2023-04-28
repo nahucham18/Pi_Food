116312dfe29d0434942e3dbe15eaf0a1237b7c04
nahucham18 <nahu.chamo19@gmail.com>
2023-04-28 10:48:02 -0300

    Style Recipe in syze Mobile

--- client/src/components/Recipe/Recipe.jsx
+++ client/src/components/Recipe/Recipe.jsx
@@ -1,32 +1,46 @@
 import { Link } from "react-router-dom";
 import style from './Recipe.module.css';
+import heart from '../../assets/heart.png'
+import star from '../../assets/star.png'
                             
 const Recipe = ({id,title,image,healthScore,diets,created}) =>{
     return (
         <div className={style.divRecipe}>
-            <Link className={style.link} to={`/detail/${id}`}>
+            
                 <div className={style.recipe}>
                     
-                    <div className={style.imageContainer}>
-                        <img className={style.image} src={image} alt={title} />
-                    </div>
-                    <div className={style.recipeTop}>
-                        <Link className={style.title} to={`/detail/${id}`}>
-                            <h2 className={style.text_h}>{title}</h2>
-                        </Link>
+                    <div className={style.leftCard}>
+                        <div className={style.imageContainer}>
+                            <img className={style.image} src={image} alt={title} />
+                        </div>
+                        <div className={style.iconsRecipe}>
+                            <div className={style.containerHeart}>
+                            <img  className={style.heart} src={heart} alt="heart" />
                             <span className={Number(healthScore) > 50 ? style.healthScore : style.nohealthScore}>{healthScore}</span>
+                            </div>
+                            <div className={style.containerStar}>
+                                <img className={style.star} src={star} alt="star.icon" />
+                            </div>
+                        </div>
                     </div>
-                    <div className={style.recipeDown}>
-                        {
-                        
-                            diets?.map((diet,index)=> {
-                                return <span key={index + 1} className={style.diet}>{diet}</span>
-                            })
-                        }
+
+                    <div className={style.description}>
+                        <div className={style.recipeTop}>
+                            <Link className={style.title} to={`/detail/${id}`}>
+                                <h2 className={style.text_h}>{title}</h2>
+                            </Link>
+                        </div>
+                        <div className={style.recipeDown}>
+                            {
+                            
+                                diets?.map((diet,index)=> {
+                                    return <span key={index + 1} className={style.diet}>{diet},</span>
+                                })
+                            }
+                        </div>
                     </div>
                     
-                </div>
-                </Link>            
+                </div>           
         </div>
     )
 }
